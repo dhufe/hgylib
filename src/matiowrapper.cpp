936b05b73d627857cf06250bf7f628590c758f3e
Daniel Kotschate (dkotscha) <daniel@d3v0.de>
2016-12-15 06:49:32 +0100

    fixed data handling and amplitude scaling.

--- src/matiowrapper.cpp
+++ src/matiowrapper.cpp
@@ -1,4 +1,5 @@
 #include <matiowrapper.h>
+#include <sstream>
 
 MatioWrapper::MatioWrapper(const std::string& _FileName)
     : szFileName(_FileName)
@@ -31,36 +32,45 @@ bool MatioWrapper::writeData(const HGFileInfo* pDataInfo, const char* pcData) {
     }
 
     double* pSig = NULL;
-    size_t pos = 0;
+    int16_t* pIData = NULL;
+    size_t nIndex = 0;
+    std::stringstream ss;
+    
     for (std::vector<HGDataType>::iterator pIter = pDataInfo->pDataTypes->begin(); pIter != pDataInfo->pDataTypes->end(); ++pIter) {
 
         if (pSig == NULL) {
             pSig = new double[pDataInfo->nSamples]();
 
+            pIData = (int16_t*)(&pcData[ (*pIter).nDataOffset ]);
+
             if ((*pIter).DataType != HGDataType::HGYVScan) {
                 for (auto y = 0; y < pDataInfo->pnDimension[1]; y++) {
                     for (auto x = 0; x < pDataInfo->pnDimension[0]; x++) {
-                        pSig  [ pDataInfo->pnDimension[0] * y + x ] = (double) ( pcData [ (*pIter).nDataOffset + pDataInfo->pnDimension[0] * y + x ] ) ;
+                        pSig[pDataInfo->pnDimension[0] * y + x] = (*pIter).dScaling * (double)(pIData[pDataInfo->pnDimension[0] * y + x]);
                     }
                 }
             } else if ( (*pIter).DataType == HGDataType::HGYVScan ) {
                 for ( auto z = 0; z < pDataInfo->pnDimension[2]; z++ ) {
                     for (auto y = 0; y < pDataInfo->pnDimension[1]; y++) {
                         for (auto x = 0; x < pDataInfo->pnDimension[0]; x++) {
-                            pSig  [ pDataInfo->pnDimension[0] * pDataInfo->pnDimension[1] * z +  pDataInfo->pnDimension[0] * y + x ] = (double) ( pcData [ (*pIter).nDataOffset + pDataInfo->pnDimension[0] * pDataInfo->pnDimension[1] * z + pDataInfo->pnDimension[0] * y + x ] ) ;
+                            pSig[pDataInfo->pnDimension[0] * pDataInfo->pnDimension[1] * z + pDataInfo->pnDimension[0] * y + x] = (*pIter).dScaling * (double)( pIData [ pDataInfo->pnDimension[0] * pDataInfo->pnDimension[1] * z + pDataInfo->pnDimension[0] * y + x]);
                         }
                     }
                 }
             }
 
-            pMatVar = Mat_VarCreate( (*pIter).toString().c_str(), MAT_C_DOUBLE, MAT_T_DOUBLE, pDataInfo->nCoordinates, pSizes, pSig, MAT_F_DONT_COPY_DATA);
+            ss << (*pIter).toString().c_str() << "_" << nIndex;
+
+            pMatVar = Mat_VarCreate( ss.str().c_str() , MAT_C_DOUBLE, MAT_T_DOUBLE, pDataInfo->nCoordinates, pSizes, pSig, MAT_F_DONT_COPY_DATA);
+            ss.clear(); ss.str("");
             Mat_VarWrite(pMat, pMatVar, MAT_COMPRESSION_NONE);
             Mat_VarFree(pMatVar);
 
             delete[] pSig;
             pSig = NULL;
+            nIndex++;
         }
     }
 
-
+    return true;
 }
