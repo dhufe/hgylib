422565524d830e09fc314a7106581e5ee221ac84
Daniel Kotschate <daniel@d3v0.de>
2019-07-09 10:09:36 +0200

    Handling large files using libmatio and h5py.

---
+++ tests/test_h5py_lfs.py
@@ -0,0 +1,18 @@
+#!/usr/env python3
+
+import h5py as hd
+
+f = hd.File ( 'blaa.mat', 'r' )
+
+print ( list( f.keys() ) )
+
+
+# Access the data without loading the whole file
+ds = f['M_PI']
+print ( ds.shape )
+print ( type(ds) )
+
+# Access an inset of the loaded data
+nA = ds[0,500:1500]
+print ( nA.shape )
+print ( type(nA) )
