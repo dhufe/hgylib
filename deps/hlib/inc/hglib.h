78494d56e2ce852c2b20f08214346111e3000ea2
Daniel Hufschläger <daniel.hufschlaeger@bam.de>
2022-11-15 12:27:25 +0100

    Initialisize members properly.

--- deps/hlib/inc/hglib.h
+++ deps/hlib/inc/hglib.h
@@ -27,7 +27,9 @@ struct HGDataType {
     HGDataType( eDataType _type )
         : DataType( _type )
         , nBytes( 0 )
+        , nDataWordSize(0)
         , nDataOffset( 0 )
+        , dScaling (0)
     {
 
     }
