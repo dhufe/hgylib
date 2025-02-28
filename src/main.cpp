98cf6ac5001bc22288e217d3c1f75dfd3696a35c
Daniel Hufschläger <daniel@hufschlaeger.net>
2025-02-28 16:24:05 +0100

    Fixed warnings generated by special characters.

--- src/main.cpp
+++ src/main.cpp
@@ -18,120 +18,122 @@
  * =====================================================================================
  */
 
-#include <cstdlib>
-#include <iostream>
-#include <hglib.h>
-#include <inttypes.h>
-#include <hlibexeception.h>
 #include <console_arg.h>
+#include <hglib.h>
+#include <hlibexeception.h>
+#include <inttypes.h>
 #include <matiowrapper.h>
 #include <version.h>
 
-int main ( int argc, char* argv[] ) {
+#include <cstdlib>
+#include <iostream>
 
-    ConArgs *pConfigArgs = new ConArgs();
+int main(int argc, char* argv[]) {
+  ConArgs* pConfigArgs = new ConArgs();
 
-    static struct option long_options[] = {
-        { "verbose", no_argument, &(pConfigArgs->verbose_flag), 1 },
-        { "export", no_argument, &(pConfigArgs->export_flag), 1 },
-        { "infile", required_argument, 0, 'i' },
-        { "outfile", required_argument, 0, 'o' },
-        { 0, 0, 0, 0 }
-    };
+  static struct option long_options[] = {
+      {"verbose", no_argument, &(pConfigArgs->verbose_flag), 1},
+      {"export", no_argument, &(pConfigArgs->export_flag), 1},
+      {"infile", required_argument, 0, 'i'},
+      {"outfile", required_argument, 0, 'o'},
+      {0, 0, 0, 0}};
 
-    if (pConfigArgs->parse_arguments_long(argc, argv, "i:o:", long_options) == -1) {
-        pConfigArgs->usage();
-        return EXIT_FAILURE;
-    } else {
-        std::cout << std::endl;
-        std::cout << pConfigArgs->szApplicationName << std::endl;
-        std::cout << std::endl;
-        std::cout << "Copyright © Daniel Hufschlaeger (dhufe)" << std::endl;
-        std::cout << std::endl;
-        std::cout << "Version info:" << std::endl;
-        std::cout << "  last commit: " << VersionInfo::GIT_DATE << std::endl;
-        std::cout << "  commit rev: " << VersionInfo::GIT_SHA1 << std::endl;
-        std::cout << "  commig msg: " << VersionInfo::GIT_COMMIT_SUBJECT << std::endl;
-        std::cout << std::endl << std::endl;
-
-        HGParser hp(pConfigArgs->szInputFileName, pConfigArgs->export_flag );
-        char* pcData = nullptr;
-        HGFileInfo* pFile = nullptr;
-
-        try {
-            hp.parseFile(&pFile);
-        }
-        catch (const HLibException& e) {
-            std::cerr << "An error was reported : " << e.what() << std::endl;
-            return EXIT_FAILURE;
-        }
-
-        if (pConfigArgs->verbose_flag) {
-
-            std::cout << "Dimensions : ";
-            for (auto i = 0; i < pFile->nCoordinates; i++) {
-                if (i < pFile->nCoordinates - 1) {
-                    std::cout << pFile->pnDimension[i] << " x ";
-                }
-                else{
-                    std::cout << pFile->pnDimension[i] << std::endl;
-                }
-            }
-
-            std::cout << "Start : ";
-            for (auto i = 0; i < pFile->nCoordinates; i++) {
-                if (i < pFile->nCoordinates - 1) {
-                    std::cout << pFile->pdStart[i] << " x ";
-                }
-                else {
-                    std::cout << pFile->pdStart[i] << std::endl;
-                }
-            }
-
-            std::cout << "Scaling : ";
-            for (auto i = 0; i < pFile->nCoordinates; i++) {
-                if (i < pFile->nCoordinates - 1) {
-                    std::cout << pFile->pdScale[i] << " x ";
-                }
-                else{
-                    std::cout << pFile->pdScale[i] << std::endl;
-                }
-            }
-
-
-            if (pFile->pDataTypes) {
-                std::cout << std::endl << pFile->pDataTypes->size() << " different types of measurement data." << std::endl;
-                int i = 0;
-                for (std::vector<HGDataType>::iterator pIter = pFile->pDataTypes->begin(); pIter != pFile->pDataTypes->end(); ++pIter) {
-                    i++;
-                    std::cout << "data set #" << i << " Datatype : " << (*pIter).toString() << " DataOffset : " << (*pIter).nDataOffset << " Size : " << (*pIter).nBytes << " bytes" << std::endl;
-                }
-            }
-
-            std::cout << "total size of data region : " << pFile->nBytes << " Bytes" << std::endl;
-        }
-        try {
-            pcData = new char[pFile->nBytes];
-        }
-        catch (std::bad_alloc) {
-            std::cerr << "unable to allocate memory!" << std::endl;
-        }
-
-        try {
-            hp.getData(pcData, &pFile);
-        }
-        catch (const HLibException& e) {
-            std::cerr << "An error was reported : " << e.what() << std::endl;
-        }
-
-        MatioWrapper wrapper(pConfigArgs->szOutputFileName);
-        wrapper.writeData(pFile, pcData);
-
-        delete[] pcData;
-        delete pConfigArgs;
-        delete pFile;
-
-        return EXIT_SUCCESS;
-    }
+  if (pConfigArgs->parse_arguments_long(argc, argv, "i:o:", long_options) ==
+      -1) {
+    pConfigArgs->usage();
     return EXIT_FAILURE;
+  } else {
+    std::cout << std::endl;
+    std::cout << pConfigArgs->szApplicationName << std::endl;
+    std::cout << std::endl;
+    std::cout << "Copyright " << '\xA9' << " Daniel Hufschlaeger (dhufe)"
+              << std::endl;
+    std::cout << std::endl;
+    std::cout << "Version info:" << std::endl;
+    std::cout << "  last commit: " << VersionInfo::GIT_DATE << std::endl;
+    std::cout << "  commit rev: " << VersionInfo::GIT_SHA1 << std::endl;
+    std::cout << "  commig msg: " << VersionInfo::GIT_COMMIT_SUBJECT
+              << std::endl;
+    std::cout << std::endl << std::endl;
+
+    HGParser hp(pConfigArgs->szInputFileName, pConfigArgs->export_flag);
+    char* pcData = nullptr;
+    HGFileInfo* pFile = nullptr;
+
+    try {
+      hp.parseFile(&pFile);
+    } catch (const HLibException& e) {
+      std::cerr << "An error was reported : " << e.what() << std::endl;
+      return EXIT_FAILURE;
+    }
+
+    if (pConfigArgs->verbose_flag) {
+      std::cout << "Dimensions : ";
+      for (auto i = 0; i < pFile->nCoordinates; i++) {
+        if (i < pFile->nCoordinates - 1) {
+          std::cout << pFile->pnDimension[i] << " x ";
+        } else {
+          std::cout << pFile->pnDimension[i] << std::endl;
+        }
+      }
+
+      std::cout << "Start : ";
+      for (auto i = 0; i < pFile->nCoordinates; i++) {
+        if (i < pFile->nCoordinates - 1) {
+          std::cout << pFile->pdStart[i] << " x ";
+        } else {
+          std::cout << pFile->pdStart[i] << std::endl;
+        }
+      }
+
+      std::cout << "Scaling : ";
+      for (auto i = 0; i < pFile->nCoordinates; i++) {
+        if (i < pFile->nCoordinates - 1) {
+          std::cout << pFile->pdScale[i] << " x ";
+        } else {
+          std::cout << pFile->pdScale[i] << std::endl;
+        }
+      }
+
+      if (pFile->pDataTypes) {
+        std::cout << std::endl
+                  << pFile->pDataTypes->size()
+                  << " different types of measurement data." << std::endl;
+        int i = 0;
+        for (std::vector<HGDataType>::iterator pIter =
+                 pFile->pDataTypes->begin();
+             pIter != pFile->pDataTypes->end(); ++pIter) {
+          i++;
+          std::cout << "data set #" << i
+                    << " Datatype : " << (*pIter).toString()
+                    << " DataOffset : " << (*pIter).nDataOffset
+                    << " Size : " << (*pIter).nBytes << " bytes" << std::endl;
+        }
+      }
+
+      std::cout << "total size of data region : " << pFile->nBytes << " Bytes"
+                << std::endl;
+    }
+    try {
+      pcData = new char[pFile->nBytes];
+    } catch (std::bad_alloc) {
+      std::cerr << "unable to allocate memory!" << std::endl;
+    }
+
+    try {
+      hp.getData(pcData, &pFile);
+    } catch (const HLibException& e) {
+      std::cerr << "An error was reported : " << e.what() << std::endl;
+    }
+
+    MatioWrapper wrapper(pConfigArgs->szOutputFileName);
+    wrapper.writeData(pFile, pcData);
+
+    delete[] pcData;
+    delete pConfigArgs;
+    delete pFile;
+
+    return EXIT_SUCCESS;
+  }
+  return EXIT_FAILURE;
 }
--- src/matiowrapper.cpp
+++ src/matiowrapper.cpp
@@ -1,148 +1,164 @@
 #include <matiowrapper.h>
+
 #include <sstream>
 
 MatioWrapper::MatioWrapper(const std::string& _FileName)
-    : szFileName(_FileName)
-    , pMat(NULL)
-    , pMatVar(NULL)
-{
-
-}
+    : szFileName(_FileName), pMat(NULL), pMatVar(NULL) {}
 
 bool MatioWrapper::writeData(const HGFileInfo* pDataInfo, const char* pcData) {
-    if (szFileName.empty())
-        return false;
+  if (szFileName.empty()) return false;
 
-    if (pDataInfo == nullptr)
-        return false;
+  if (pDataInfo == nullptr) return false;
 
-    if (pcData == nullptr)
-        return false;
+  if (pcData == nullptr) return false;
 
+  pMat = Mat_CreateVer(szFileName.c_str(), NULL, MAT_FT_MAT73);
 
-    pMat = Mat_CreateVer(szFileName.c_str() , NULL, MAT_FT_MAT73);
+  if (pMat == NULL) return false;
 
-    if (pMat == NULL)
-        return false;
+  // size of matrices
+  size_t* pSizes = new size_t[pDataInfo->nCoordinates];
+  for (auto i = 0; i < pDataInfo->nCoordinates; i++) {
+    pSizes[i] = pDataInfo->pnDimension[i];
+  }
 
-    // size of matrices
-    size_t* pSizes = new size_t[pDataInfo->nCoordinates];
-    for (auto i = 0; i < pDataInfo->nCoordinates; i++) {
-        pSizes[i] = pDataInfo->pnDimension[i];
-    }
+  double* pSig = NULL;
+  int16_t* pIData = NULL;
+  int32_t* pI32Data = NULL;
+  size_t nIndex = 0;
 
-    double* pSig      = NULL;
-    int16_t* pIData   = NULL;
-    int32_t* pI32Data = NULL;
-    size_t nIndex     = 0;
+  bool bIsRotary = false;
 
-    bool bIsRotary    = false;
+  // reconstruction parameters
+  size_t sDim[2] = {1, pDataInfo->nCoordinates};
+  double* pdStart = new double[pDataInfo->nCoordinates];
+  double* pdScale = new double[pDataInfo->nCoordinates];
+  double* pdSizes = new double[pDataInfo->nCoordinates];
+  double* pnSizes = new double[pDataInfo->nCoordinates];
+  char* pcUnits = new char[pDataInfo->nCoordinates];
+  int* piRotary = new int[pDataInfo->nCoordinates];
 
-    // reconstruction parameters
-    size_t  sDim[2] = { 1, pDataInfo->nCoordinates };
-    double* pdStart  = new double[pDataInfo->nCoordinates];
-    double* pdScale  = new double[pDataInfo->nCoordinates];
-    double* pdSizes  = new double[pDataInfo->nCoordinates];
-    double* pnSizes  = new double[pDataInfo->nCoordinates];
-    char*   pcUnits  = new char[pDataInfo->nCoordinates];
-    int*   piRotary  = new int[pDataInfo->nCoordinates];
+  // struct dimensions
+  size_t dims_struct[2] = {1, 1};
+  std::stringstream ss;
 
-    // struct dimensions
-    size_t  dims_struct[2] = { 1, 1 };
-    std::stringstream ss;
+  for (std::vector<HGDataType>::iterator pIter = pDataInfo->pDataTypes->begin();
+       pIter != pDataInfo->pDataTypes->end(); ++pIter) {
+    if (pSig == NULL) {
+      pSig = new double[pDataInfo->nSamples]();
 
-    for (std::vector<HGDataType>::iterator pIter = pDataInfo->pDataTypes->begin(); pIter != pDataInfo->pDataTypes->end(); ++pIter) {
+      switch ((*pIter).nDataWordSize) {
+        default:
+        case 2:
+          pIData = (int16_t*)(&pcData[(*pIter).nDataOffset]);
+          break;
+        case 4:
+          pI32Data = (int32_t*)(&pcData[(*pIter).nDataOffset]);
+          break;
+      }
 
-        if (pSig == NULL) {
-            pSig = new double[pDataInfo->nSamples]();
-
+      if ((*pIter).DataType != HGDataType::HGYVScan) {
+        for (auto y = 0; y < pDataInfo->pnDimension[1]; y++) {
+          for (auto x = 0; x < pDataInfo->pnDimension[0]; x++) {
             switch ((*pIter).nDataWordSize) {
+              default:
+              case 2:
+                pSig[pDataInfo->pnDimension[0] * y + x] =
+                    (*pIter).dScaling *
+                    (double)(pIData[pDataInfo->pnDimension[0] * y + x]);
+                break;
+              case 4:
+                pSig[pDataInfo->pnDimension[0] * y + x] =
+                    (*pIter).dScaling *
+                    (double)(pI32Data[pDataInfo->pnDimension[0] * y + x]);
+                break;
+            }
+          }
+        }
+      } else if ((*pIter).DataType == HGDataType::HGYVScan) {
+        for (auto z = 0; z < pDataInfo->pnDimension[2]; z++) {
+          for (auto y = 0; y < pDataInfo->pnDimension[1]; y++) {
+            for (auto x = 0; x < pDataInfo->pnDimension[0]; x++) {
+              switch ((*pIter).nDataWordSize) {
                 default:
                 case 2:
-                    pIData = (int16_t*)(&pcData[(*pIter).nDataOffset]);
-                    break;
+                  pSig[pDataInfo->pnDimension[0] * pDataInfo->pnDimension[1] *
+                           z +
+                       pDataInfo->pnDimension[0] * y + x] =
+                      (*pIter).dScaling *
+                      (double)(pIData[pDataInfo->pnDimension[0] *
+                                          pDataInfo->pnDimension[1] * z +
+                                      pDataInfo->pnDimension[0] * y + x]);
+                  break;
                 case 4:
-                    pI32Data = (int32_t*)(&pcData[(*pIter).nDataOffset]);
-                    break;
+                  pSig[pDataInfo->pnDimension[0] * pDataInfo->pnDimension[1] *
+                           z +
+                       pDataInfo->pnDimension[0] * y + x] =
+                      (*pIter).dScaling *
+                      (double)(pI32Data[pDataInfo->pnDimension[0] *
+                                            pDataInfo->pnDimension[1] * z +
+                                        pDataInfo->pnDimension[0] * y + x]);
+                  break;
+              }
             }
-
-            if ((*pIter).DataType != HGDataType::HGYVScan) {
-                for (auto y = 0; y < pDataInfo->pnDimension[1]; y++) {
-                    for (auto x = 0; x < pDataInfo->pnDimension[0]; x++) {
-                        switch ((*pIter).nDataWordSize) {
-                            default:
-                            case 2:
-                                pSig[pDataInfo->pnDimension[0] * y + x] = (*pIter).dScaling * (double)(pIData[pDataInfo->pnDimension[0] * y + x]);
-                                break;
-                            case 4:
-                                pSig[pDataInfo->pnDimension[0] * y + x] = (*pIter).dScaling * (double)(pI32Data[pDataInfo->pnDimension[0] * y + x]);
-                                break;
-                        }
-                    }
-                }
-            } else if ( (*pIter).DataType == HGDataType::HGYVScan ) {
-                for ( auto z = 0; z < pDataInfo->pnDimension[2]; z++ ) {
-                    for (auto y = 0; y < pDataInfo->pnDimension[1]; y++) {
-                        for (auto x = 0; x < pDataInfo->pnDimension[0]; x++) {
-                            switch ((*pIter).nDataWordSize) {
-                                default:
-                                case 2:
-                                    pSig[pDataInfo->pnDimension[0] * pDataInfo->pnDimension[1] * z + pDataInfo->pnDimension[0] * y + x] = (*pIter).dScaling * (double)(pIData[pDataInfo->pnDimension[0] * pDataInfo->pnDimension[1] * z + pDataInfo->pnDimension[0] * y + x]);
-                                    break;
-                                case 4:
-                                    pSig[pDataInfo->pnDimension[0] * pDataInfo->pnDimension[1] * z + pDataInfo->pnDimension[0] * y + x] = (*pIter).dScaling * (double)(pI32Data[pDataInfo->pnDimension[0] * pDataInfo->pnDimension[1] * z + pDataInfo->pnDimension[0] * y + x]);
-                                    break;
-                            }
-                        }
-                    }
-                }
-            }
-
-            ss << (*pIter).toString().c_str() << "_" << nIndex;
-
-            pMatVar = Mat_VarCreate( ss.str().c_str() , MAT_C_DOUBLE, MAT_T_DOUBLE, (int) (pDataInfo->nCoordinates), pSizes, pSig, MAT_F_DONT_COPY_DATA);
-            ss.clear(); ss.str("");
-            Mat_VarWrite(pMat, pMatVar, MAT_COMPRESSION_ZLIB);
-            Mat_VarFree(pMatVar);
-
-            delete[] pSig;
-            pSig = NULL;
-            nIndex++;
+          }
         }
+      }
 
+      ss << (*pIter).toString().c_str() << "_" << nIndex;
+
+      pMatVar = Mat_VarCreate(ss.str().c_str(), MAT_C_DOUBLE, MAT_T_DOUBLE,
+                              (int)(pDataInfo->nCoordinates), pSizes, pSig,
+                              MAT_F_DONT_COPY_DATA);
+      ss.clear();
+      ss.str("");
+      Mat_VarWrite(pMat, pMatVar, MAT_COMPRESSION_ZLIB);
+      Mat_VarFree(pMatVar);
+
+      delete[] pSig;
+      pSig = NULL;
+      nIndex++;
     }
+  }
 
-    for (auto i = 0; i < pDataInfo->nCoordinates; i++) {
-        pnSizes[i] = (double) (pDataInfo->pnDimension[i]);
-        pdSizes[i] = (double)(pDataInfo->pdStart[i] + pDataInfo->pnDimension[i] * pDataInfo->pdScale[i]);
-        pcUnits[i] = pDataInfo->pcUnits[i];
-        if (pcUnits[i] == '°')
-            piRotary[i] = 1;
-        else
-            piRotary[i] = 0;
-    }
+  for (auto i = 0; i < pDataInfo->nCoordinates; i++) {
+    pnSizes[i] = (double)(pDataInfo->pnDimension[i]);
+    pdSizes[i] = (double)(pDataInfo->pdStart[i] +
+                          pDataInfo->pnDimension[i] * pDataInfo->pdScale[i]);
+    pcUnits[i] = pDataInfo->pcUnits[i];
+    if (pcUnits[i] == '\xB0')
+      piRotary[i] = 1;
+    else
+      piRotary[i] = 0;
+  }
 
-    pMatVar = Mat_VarCalloc();
-    matvar_t** ppMatStructVars = (matvar_t**)calloc(6 , sizeof(matvar_t*));
+  pMatVar = Mat_VarCalloc();
+  matvar_t** ppMatStructVars = (matvar_t**)calloc(6, sizeof(matvar_t*));
 
-    ppMatStructVars[0] = Mat_VarCreate("Scale", MAT_C_DOUBLE, MAT_T_DOUBLE, 2, sDim, (void*)(pDataInfo->pdScale), 0);
-    ppMatStructVars[1] = Mat_VarCreate("Start", MAT_C_DOUBLE, MAT_T_DOUBLE, 2, sDim, (void*)(pDataInfo->pdStart), 0);
-    ppMatStructVars[2] = Mat_VarCreate("Dimensions", MAT_C_DOUBLE, MAT_T_DOUBLE, 2, sDim, (void*)(pnSizes), 0);
-    ppMatStructVars[3] = Mat_VarCreate("Sizes", MAT_C_DOUBLE, MAT_T_DOUBLE, 2, sDim, (void*)(pdSizes), 0);
-    ppMatStructVars[4] = Mat_VarCreate("Rotary", MAT_C_INT32, MAT_T_INT32, 2, sDim, (void*)(piRotary), 0);
-    ppMatStructVars[5] = NULL;
+  ppMatStructVars[0] = Mat_VarCreate("Scale", MAT_C_DOUBLE, MAT_T_DOUBLE, 2,
+                                     sDim, (void*)(pDataInfo->pdScale), 0);
+  ppMatStructVars[1] = Mat_VarCreate("Start", MAT_C_DOUBLE, MAT_T_DOUBLE, 2,
+                                     sDim, (void*)(pDataInfo->pdStart), 0);
+  ppMatStructVars[2] = Mat_VarCreate("Dimensions", MAT_C_DOUBLE, MAT_T_DOUBLE,
+                                     2, sDim, (void*)(pnSizes), 0);
+  ppMatStructVars[3] = Mat_VarCreate("Sizes", MAT_C_DOUBLE, MAT_T_DOUBLE, 2,
+                                     sDim, (void*)(pdSizes), 0);
+  ppMatStructVars[4] = Mat_VarCreate("Rotary", MAT_C_INT32, MAT_T_INT32, 2,
+                                     sDim, (void*)(piRotary), 0);
+  ppMatStructVars[5] = NULL;
 
-    matvar_t*  pMatStruct = Mat_VarCreate("recparm", MAT_C_STRUCT, MAT_T_STRUCT, 2, dims_struct, ppMatStructVars, 0);
-    Mat_VarWrite(pMat, pMatStruct, MAT_COMPRESSION_ZLIB);
-    Mat_VarFree(pMatVar);
-    Mat_Close(pMat);
+  matvar_t* pMatStruct = Mat_VarCreate("recparm", MAT_C_STRUCT, MAT_T_STRUCT, 2,
+                                       dims_struct, ppMatStructVars, 0);
+  Mat_VarWrite(pMat, pMatStruct, MAT_COMPRESSION_ZLIB);
+  Mat_VarFree(pMatVar);
+  Mat_Close(pMat);
 
-    delete[] pdStart;
-    delete[] pdScale;
-    delete[] pdSizes;
-    delete[] pnSizes;
-    delete[] pcUnits;
-    delete[] piRotary;
+  delete[] pdStart;
+  delete[] pdScale;
+  delete[] pdSizes;
+  delete[] pnSizes;
+  delete[] pcUnits;
+  delete[] piRotary;
 
-    return true;
+  return true;
 }
